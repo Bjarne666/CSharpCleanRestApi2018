ed806ac5bf19bed4ef5cf13401f94b6094ab1c6c
Bjarne666 <Bjarne96@outlook.dk>
2019-09-03 15:57:59 +0200

    Initial commit

---
+++ CustomerApp.UI.RestApi/Controllers/PetsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CustomerApp.Core.Entity;
+using CustomerApp.Core;
+using CustomerApp.Core.ApplicationService;
+using CustomerApp.Infrastructure.Static.Data.Repositories;
+
+namespace CustomerApp.UI.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            customerService = _customerService;
+        }
+
+        // Get api/pet
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> Get()
+
+        {
+            return _customerService.GetAllCustomers();
+        }
+    }
+}
